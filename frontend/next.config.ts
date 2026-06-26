91e489562cc46a7d130d9699c26f2b5248204fb5
Kelvin Kang <kji9623@gmail.com>
2026-06-26 16:47:04 +0900

    Revert Next.js config since deploying to Vercel

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -1,19 +1,7 @@
 import type { NextConfig } from "next";
 
-const isGithubActions = process.env.GITHUB_ACTIONS || false;
-
-let basePath = '';
-
-if (isGithubActions && process.env.GITHUB_REPOSITORY) {
-  const repo = process.env.GITHUB_REPOSITORY.replace(/.*?\//, '');
-  basePath = `/${repo}`;
-}
-
 const nextConfig: NextConfig = {
   devIndicators: false,
-  output: "export",
-  basePath: basePath,
-  assetPrefix: basePath,
 };
 
 export default nextConfig;
